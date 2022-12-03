328999a9f04c4cfaf58ef39364a5d9f860ed5ce9
Mo'men <codermanbrains@gmail.com>
2022-12-03 10:39:08 +0200

    - Radio Screen with local file - Sebha Rotate when counter increase

--- lib/home_screen/home_screen_tabs/radio_tab.dart
+++ lib/home_screen/home_screen_tabs/radio_tab.dart
@@ -2,16 +2,144 @@
 
 
 
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:islami_assignment_moamen/ThemeOfData.dart';
 
-class radio extends StatelessWidget {
+class radio extends StatefulWidget {
 
   static const String routeName = "radio";
 
   @override
+  State<radio> createState() => _radioState();
+}
+
+class _radioState extends State<radio> {
+
+
+  final audioPlayer = AudioPlayer();
+  bool itsPlaying = false;
+  Duration duration = Duration.zero;
+  Duration postion = Duration.zero;
+
+
+  @override
+  void initState(){
+    super.initState();
+
+    setAudio();
+
+    audioPlayer.onPlayerStateChanged.listen((event) {
+      setState(() {
+        itsPlaying = event == PlayerState.playing;
+      });
+    });
+
+
+    audioPlayer.onDurationChanged.listen((event) {
+      setState(() {
+        duration = event;
+      });
+    });
+
+    audioPlayer.onPositionChanged.listen((event) {
+      setState(() {
+        postion = event;
+      });
+    });
+
+  }
+
+
+  @override
+  void dispose(){
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+
+
+
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius:BorderRadius.circular(20),
+          child: Image.asset("assets/images/radio_image.png",width: double.infinity,height: 200,fit: BoxFit.cover,),
+        ),
+        Text(AppLocalizations.of(context)!.elbaqra,style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+        Text(AppLocalizations.of(context)!.mesharyalafasy,style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal),),
+        Slider(
+            min : 0,
+            max: duration.inSeconds.toDouble(),
+            value: postion.inSeconds.toDouble(),
+            onChanged: (value) async {
+              final postion = Duration(seconds: value.toInt());
+              await audioPlayer.seek(postion);
+
+
+              await audioPlayer.resume();
+
+        }),
+        Padding(padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(formatTime(postion)),
+              Text(formatTime(duration - postion))
+            ],
+          ),
+        ),
+        CircleAvatar(
+          radius: 45,
+          backgroundColor: ThemeOfData.colorGold,
+          child: CircleAvatar(
+            radius: 40,
+            backgroundColor: Color(0xfffaf5ef),
+            foregroundColor: ThemeOfData.colorGold,
+            child: IconButton(
+              icon: Icon(
+                itsPlaying ? Icons.pause : Icons.play_arrow,
+              ),
+              iconSize: 50,
+              onPressed: () async {
+                if(itsPlaying){
+                  await audioPlayer.pause();
+                } else {
+            await audioPlayer.resume();
+                }
+              },
+            ),
+          ),
+        )
+      ],
     );
   }
+
+  String formatTime(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2,'0');
+
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+
+    return [
+    if (duration.inHours > 0 ) hours,
+      minutes,
+      seconds
+    ].join(':');
+  }
+
+  void setAudio() async{
+    audioPlayer.setReleaseMode(ReleaseMode.loop);
+
+   final player = AudioCache(prefix: 'assets/audio/');
+   final url = await player.load('baqra.mp3');
+   audioPlayer.setSourceUrl(url.path, );
+
+  }
+
 }
--- lib/home_screen/home_screen_tabs/sebha_tab.dart
+++ lib/home_screen/home_screen_tabs/sebha_tab.dart
@@ -1,56 +1,126 @@
 
+import 'dart:math' as math;
+import 'dart:math';
 
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:islami_assignment_moamen/ThemeOfData.dart';
 
-class sebha extends StatefulWidget  {
+class sebha extends StatefulWidget {
 
   static const String routeName = "sebha";
 
+
   @override
   State<sebha> createState() => _sebhaState();
-
-
 }
 
-class _sebhaState extends State<sebha> with TickerProviderStateMixin {
+class _sebhaState extends State<sebha> with SingleTickerProviderStateMixin {
+
+  int counter = 0;
+  double angleValue = 0.0;
+
+  List<String> Tasebhat = [
+    "سبحان الله",
+    "الحمدلله",
+    "الله اكبر",
+    "استغفر الله",
+  ];
+
+  String? _dropDownValue ;
+
+  void _incrementCounter() {
+
+    if(_dropDownValue!.isEmpty){
+      counter=0;
+      _dropDownValue = null;
+    }
+    if (_dropDownValue!.isNotEmpty && counter < 34) {
+        counter++;
+        angleValue++;
+    }
+
+    if (_dropDownValue!.isNotEmpty && counter == 33) {
+      counter = 0;
+      _dropDownValue = null;
+    }
+      setState(() {
+
+      });
+    // counter = 0;
+  }
+
+
+  void dropDownCallback(Object? selectionValue) {
+    if(selectionValue is String){
+      setState(() {
+        _dropDownValue = selectionValue;
+      });
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
 
-    late final AnimationController _controller = AnimationController(
-      duration: const Duration(seconds: 22),
-      vsync: this,
-    )..repeat(reverse: false);
-    late final Animation<double> _animation = CurvedAnimation(
-      parent: _controller,
-      curve: Curves.linear,
-    );
+       return Column(
+         children: [
+           Stack(
+             children: [
 
-    @override
-    void dispose() {
-      _controller.dispose();
-      super.dispose();
-    }
+               Padding(
+                 padding: const EdgeInsets.only(left: 45,bottom: 18.0),
+                 child: Center(
+                   child: Image.asset("assets/images/head_sebha_logo.png"),
+                 ),
+               ),
+
+               Padding(
+                 padding: const EdgeInsets.symmetric(vertical: 68.0,horizontal: 85),
+                 child: Center(
+                     child: Transform.rotate(
+                         angle: angleValue,
+                         child: Image.asset("assets/images/body_sebha_logo.png")
+                     )
+                 ),
+               ),
+
+             ],
+           ),
+
+           Center(
+             child: Text("${counter}"
+               ,style: TextStyle(color: ThemeOfData.colorBlack,fontSize: 25),
+             ),
+           ),
+
+          DropdownButton<String>(items: Tasebhat.map(buildMenuItem).toList(),
+              value: _dropDownValue,
+              onChanged: (value) => setState(()  => this._dropDownValue = value)
+          ),
+          
+           ElevatedButton(onPressed: _incrementCounter ,
+               child: Text(AppLocalizations.of(context)!.tasbeha,style: TextStyle(color: ThemeOfData.colorGold),),
+           style: ElevatedButton.styleFrom(
+             elevation: 0,
+             backgroundColor: Colors.transparent,
+    side: BorderSide(width: 2.0, color: ThemeOfData.colorGold,),
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(30),
+
+        ),
+    )
+           )
+         ],
+       );
 
 
-    return Column(
-      children: [
-        Center(
-          child: Image.asset("assets/images/head_sebha_logo.png"),
-        )
-        ,
+     }
 
-        RotationTransition(alignment: Alignment.topLeft,
-        turns: _animation,
-        child: Image.asset("assets/images/body_sebha_logo.png"))
-        ,
-        Center(
-          child: Text("عدد التسبيحات"
-          ,style: TextStyle(color: ThemeOfData.colorBlack,fontSize: 25),),
-        )
-      ],
-    );
-  }
+     DropdownMenuItem<String> buildMenuItem(String item)  => DropdownMenuItem(
+         value: item,
+         child: Text(item),
+
+     );
 }
+
